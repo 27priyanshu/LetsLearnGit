6a4bcc1c904bc73c53900cfc1422ec410b3e3c47
Priyanshu <2003priyanshusingh@gmail.com>
2022-01-21 20:27:49 +0530

    initial commit

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a,b,s;
+    printf("Enter the value of a and b \n");
+    scanf("%d%d",&a,&b);
+    s=a+b;
+    printf("sum of numbers:a+b=%d",s);
+    return 0;
+}
